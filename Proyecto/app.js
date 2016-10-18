a751b499e3d8a33299d47164ed750b1df9f21b56
Julio <fcojulio@gmail.com>
2016-10-18 15:29:07 +0200

    Test connection mongodb

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -1,11 +1,26 @@
 
-var express = require("express");
-var app = express();
+var express 	= require("express");
+var bodyParser 	= require('body-parser');
+var mongoose	= require("mongoose");
 
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/fotos");
+
+var userSchemaJSON = {
+	email:String,
+	password:String
+};
+
+var app 		= express();
+var user_schema = new mongoose.Schema(userSchemaJSON);
+var User 		= mongoose.model("User", user_schema);
 
 
 app.use("/static",express.static('./public'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set("view engine", "jade");
 
 app.get("/", function(req,res) {
@@ -13,8 +28,24 @@ app.get("/", function(req,res) {
 	res.render("index");
 })
 
-app.get("/login", function(req,res){
-	res.render("login");
+app.get("/login", function(req,res){	
+	User.find(function(err,doc){
+		console.log(doc);
+		res.render("login");
+	});
+	
+});
+
+app.post("/users", function(req, res){
+	//console.log("E-mail: " + req.body.email);
+	//console.log("Contraseña: " + req.body.pass);
+	
+	var user = new User({email: req.body.email, password: req.body.pass});
+	user.save(function(){
+		res.send("Guardamos tus datos");
+	});
+	
+
 });
 
 app.listen(8080);
